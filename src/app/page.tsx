f76615126ab4c40296597549c42c8448a0099562
thyagomac <thyago.cormick@gmail.com>
2024-08-22 12:01:52 -0300

    implement add, remove, sort;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,76 @@
 "use client"
 
 import { usePeopleList } from "@/reducers/peopleList";
+import { ChangeEvent, useState } from "react";
 
 export default function Home() {
   const [peopleList, dispatchPeopleList] = usePeopleList();
+  const [name, setName] = useState<string>("");
+
+  const handleAddPeopleBtn = () => {
+    if(!name) return;
+    dispatchPeopleList({
+      type: "ADD",
+      payload: {
+        name: name
+      }
+    })
+    setName("");
+  }
+
+  const handleDeletePeopleBtn = (id: string) => {
+    if(!id) return;
+
+    dispatchPeopleList({
+      type: "DEL",
+      payload: {
+        id: id
+      }
+    })
+  }
+
+  const handleSortPeopleBtn = () => {
+    dispatchPeopleList({type: "SORT"})
+  }
+
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value)
+  }
 
   return (
-    <main className="min-h-screen">
-      <h1>People list:</h1>
-      {peopleList.map((people) => (
-        <li key={people.id}>{people.name}</li>
-      ))
+    <main className="max-w-5xl min-h-screen m-auto p-10">
+      <div className="flex flex-col gap-4 items-center justify-center">
+        <div className="flex items-center justify-center">
+          <input
+            className="p-2 bg-white text-black rounded-md"
+            type="text"
+            value={name}
+            onChange={handleNameChange}
+          />
+          <button
+            className="px-3 py-2 bg-blue-500 rounded-md ml-1"
+            onClick={handleAddPeopleBtn}
+          >
+            Add People
+          </button>
+          <button
+            className="px-3 py-2 bg-blue-500 rounded-md ml-1"
+            onClick={handleSortPeopleBtn}
+          >
+            Sort People
+          </button>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <h1>People list</h1>
+          {peopleList.map((people) => (
+            <li className="my-2" key={people.id}>
+              {people.name}
+              <button className="ml-2 px-2 py-1 bg-red-700 rounded-md" onClick={() => handleDeletePeopleBtn(people.id)}>Delete</button>
+            </li>
+          ))}
+        </div>
+      </div>
 
-      }
     </main>
   );
 }
--- src/reducers/peopleList.ts
+++ src/reducers/peopleList.ts
@@ -3,7 +3,7 @@ import { useReducer } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 type ActionType = {
-  type: string;
+  type: "ADD" | "DEL" | "SORT";
   payload?: {
     id?: string;
     name?: string;
@@ -16,28 +16,39 @@ const reducer = (state: PersonType[], action: ActionType) => {
   switch (action.type) {
     case "ADD":
         if(action.payload?.name) {
-          state.push({
-            id: uuidv4(),
-            name: action.payload?.name
-          })
+          const newState = [
+            ...state,
+            {
+              id: uuidv4(),
+              name: action.payload?.name
+            }
+          ]
+          
+          return newState;
         }
       break;
   
     case "DEL":
       if(action.payload?.id) {
-        state = state.filter(
+        let newState = [...state];
+        newState = newState.filter(
           item => item.id !== action.payload?.id
         );
+
+        return newState;
       }
 
       break;
 
-    case "ORDER":
-      //state = state.sort((a, b) => (a.name > b.name) ? 1 : -1);
-      state = state.sort((a, b) => a.name.localeCompare(b.name));
+    case "SORT":
+      let newState = [...state]
+      //newState = newState.sort((a, b) => (a.name > b.name) ? 1 : -1);
+      newState = newState.sort((a, b) => a.name.localeCompare(b.name));
 
+      return newState;
       break;
   }
+
   return state;
 }
 
